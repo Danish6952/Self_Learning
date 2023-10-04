e9b7e4bab34763c5d9ef45a35ae5a1e275a797d3
Raja Danish <rajadanish52286@gmail.com>
2023-10-04 17:22:08 +0500

    python learning

---
+++ script.py
@@ -0,0 +1,12 @@
+print("HELLO WORLD")   #This will print a string
+print("MY NAME IS DANISH")
+print("I started learning python")
+print(3+4)             #This will print addition of 2 numbers
+
+
+#VARIABLES
+x = 3+4*9
+print(x)
+x+4
+
+# ctrl + /         pressing both will comment out a line of code
